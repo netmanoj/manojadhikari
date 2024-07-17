879d8f6b050e3fac2fa4a8fce686112813b6de59
manoj-adhikari <imadhikarimanoj@gmail.com>
2024-07-18 02:15:35 +0530

    fixed typewriter text issue

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+  setTimeout(() => {
+      const line1 = document.getElementById('line1');
+      const line2 = document.getElementById('line2');
+      line1.style.borderRight = 'none';
+      line2.style.opacity = '1';
+      line2.style.borderRight = '.15em solid orange';
+      line2.style.animation = 'typing 3.5s steps(40, end) forwards, blink-caret .75s step-end infinite';
+  }, 3500); // Wait for the first line to finish typing
+});
 
+// for sound cards
 window.addEventListener("load", () => {
     const sounds = document.querySelectorAll(".sound");
     const pads = document.querySelectorAll(".pads div");
